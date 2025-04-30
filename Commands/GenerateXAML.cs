f7286608c039cb60dd52128d63000c6b7716840f
Andrew <anki_06@mail.ru>
2025-04-30 15:26:26 +0300

    Added GenerateXaml

---
+++ Commands/GenerateXAML.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Windows;
+using DiplomProject.Dialogs;
+using DiplomProject.Helper;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace DiplomProject
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class GenerateXAML
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("6cd20ec3-b75b-4028-937d-0f4dcadcfa4e");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenerateXAML"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private GenerateXAML(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static GenerateXAML Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new GenerateXAML(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            string title = "GenerateXAML";
+
+            var dialog = new Window
+            {
+                Title = title,
+                Width = 400,
+                Height = 300,
+                Content = new SelectionControl(),
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            };
+
+            if (dialog.Content is SelectionControl control)
+            {
+                control.OkClicked += (s, args) =>
+                {
+                    var xamlGenerator = new XamlGenerator();
+                    xamlGenerator.GenerateXaml(control.SelectedModel,
+                         control.GenerateViewModel,
+                         control.UseDataBinding,
+                         control.AddValidation);
+                    dialog.Close();
+                };
+
+                control.CancelClicked += (s, args) => dialog.Close();
+            }
+
+            dialog.ShowDialog();
+        }
+    }
+}
