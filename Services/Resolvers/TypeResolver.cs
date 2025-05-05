356c64dd47c2fbed739ce8caee2b89a414a4ade2
Andrew <anki_06@mail.ru>
2025-05-05 12:57:16 +0300

    Added class Resolvers

---
+++ Services/Resolvers/TypeResolver.cs
@@ -0,0 +1,15 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+
+namespace DiplomProject.Services.Resolver
+{
+
+    public class TypeResolver
+    {
+        public string GetPropertyType(CodeProperty property)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            return property.Type.AsString;
+        }
+    }
+}
