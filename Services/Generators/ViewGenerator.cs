654e5c9fa77058fb5405ac3d53c1381e8fa850db
Andrew <anki_06@mail.ru>
2025-05-05 12:56:39 +0300

    Added class Generators

---
+++ Services/Generators/ViewGenerator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiplomProject.Services.Builder;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace DiplomProject.Services.Generators
+{
+    public class ViewGenerator
+    {
+        private readonly AsyncPackage _package;
+        private readonly XamlBuilder _xamlBuilder;
+
+        public ViewGenerator(AsyncPackage package)
+        {
+            _package = package ?? throw new ArgumentNullException(nameof(package));
+            _xamlBuilder = new XamlBuilder();
+        }
+
+        public void GenerateViewFiles(CodeClass modelClass, ProjectItem targetProjectItem, bool isUseDataBinding)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                string projectPath = Path.GetDirectoryName(targetProjectItem.ContainingProject.FullName);
+                string viewsFolder = Path.Combine(projectPath, "Views");
+
+                if (!Directory.Exists(viewsFolder))
+                {
+                    Directory.CreateDirectory(viewsFolder);
+                }
+
+                string viewName = $"{modelClass.Name}View";
+                var (xamlContent, csContent) = _xamlBuilder.BuildViewContent(modelClass, isUseDataBinding);
+
+                SaveAndAddToProject(
+                    targetProjectItem,
+                    viewsFolder,
+                    $"{viewName}.xaml",
+                    xamlContent,
+                    $"{viewName}.xaml.cs",
+                    csContent,
+                    "Views");
+
+                ShowSuccessMessage($"View for {modelClass.Name} successfully generated!");
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage($"Error generating View: {ex.Message}");
+            }
+        }
+
+        private void SaveAndAddToProject(
+            ProjectItem targetProjectItem,
+            string folderPath,
+            string fileName1,
+            string content1,
+            string fileName2,
+            string content2,
+            string folderName)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string filePath1 = Path.Combine(folderPath, fileName1);
+            File.WriteAllText(filePath1, content1);
+
+            string filePath2 = Path.Combine(folderPath, fileName2);
+            File.WriteAllText(filePath2, content2);
+
+            var folderItem = targetProjectItem.ContainingProject.ProjectItems
+                .Cast<ProjectItem>()
+                .FirstOrDefault(i =>
+                {
+                    ThreadHelper.ThrowIfNotOnUIThread();
+                    return i.Name == folderName;
+                }) ?? targetProjectItem.ContainingProject.ProjectItems.AddFolder(folderName);
+
+            folderItem.ProjectItems.AddFromFile(filePath1);
+        }
+
+        private void ShowSuccessMessage(string message)
+        {
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                message,
+                "Success",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                message,
+                "Error",
+                OLEMSGICON.OLEMSGICON_CRITICAL,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ Services/Generators/ViewModelGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiplomProject.Services.Builder;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace DiplomProject.Services.Generators
+{
+    public class ViewModelGenerator
+    {
+        private readonly AsyncPackage _package;
+        private readonly ViewModelBuilder _viewModelBuilder;
+
+        public ViewModelGenerator(AsyncPackage package)
+        {
+            _package = package ?? throw new ArgumentNullException(nameof(package));
+            _viewModelBuilder = new ViewModelBuilder();
+        }
+
+        public void GenerateViewModel(CodeClass modelClass, ProjectItem targetProjectItem)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                string projectPath = Path.GetDirectoryName(targetProjectItem.ContainingProject.FullName);
+                string viewModelsFolder = Path.Combine(projectPath, "ViewModels");
+
+                if (!Directory.Exists(viewModelsFolder))
+                {
+                    Directory.CreateDirectory(viewModelsFolder);
+                }
+
+                string viewModelContent = _viewModelBuilder.BuildViewModelContent(modelClass);
+                string viewModelName = $"{modelClass.Name}ViewModel.cs";
+                string viewModelPath = Path.Combine(viewModelsFolder, viewModelName);
+
+                File.WriteAllText(viewModelPath, viewModelContent);
+
+                var viewModelsFolderItem = targetProjectItem.ContainingProject.ProjectItems
+                    .Cast<ProjectItem>()
+                    .FirstOrDefault(i =>
+                    {
+                        ThreadHelper.ThrowIfNotOnUIThread();
+                        return i.Name == "ViewModels";
+                    }) ?? targetProjectItem.ContainingProject.ProjectItems.AddFolder("ViewModels");
+
+                viewModelsFolderItem.ProjectItems.AddFromFile(viewModelPath);
+
+                ShowSuccessMessage($"ViewModel for {modelClass.Name} successfully generated!");
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage($"Error generating ViewModel: {ex.Message}");
+            }
+        }
+
+        private void ShowSuccessMessage(string message)
+        {
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                message,
+                "Success",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                message,
+                "Error",
+                OLEMSGICON.OLEMSGICON_CRITICAL,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
