f39a57e0871312deb235c4653c896e20ebcf59ee
Andrew <anki_06@mail.ru>
2025-05-06 11:33:19 +0300

    Added Message service

---
+++ Services/MessageService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+using DiplomProject.Services.Builder;
+
+namespace DiplomProject.Services
+{
+    public class MessageService
+    {
+        private readonly AsyncPackage _package;
+
+        public MessageService(AsyncPackage package)
+        {
+            _package = package ?? throw new ArgumentNullException(nameof(package));
+        }
+
+        public void ShowSuccessMessage(string message)
+        {
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                message,
+                "Success",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        public void ShowErrorMessage(string message)
+        {
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                message,
+                "Error",
+                OLEMSGICON.OLEMSGICON_CRITICAL,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
