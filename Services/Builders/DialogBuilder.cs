6aea531292a566bc2058ef56d494a1c8b39fa77f
Andrew <anki_06@mail.ru>
2025-05-12 15:00:59 +0300

    Added generate Dialog view and viewModel for adding and editing

---
+++ Services/Builders/DialogBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiplomProject.Services.Builder;
+using DiplomProject.Services.Resolver;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace DiplomProject.Services.Builders
+{
+    public class DialogBuilder
+    {
+        private readonly NamespaceResolver _namespaceResolver = new NamespaceResolver();
+        private readonly PropertyControlBuilder _propertyControlBuilder = new PropertyControlBuilder();
+
+        public (string xamlContent, string csContent) BuildDialogContent(CodeClass codeClass, bool isUseDataBinding)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var namespaces = _namespaceResolver.GetProjectNamespace(codeClass);
+
+            string xamlContent = BuildXamlDialog(codeClass, namespaces, isUseDataBinding);
+
+            string csContent = BuildCsDialog(codeClass, namespaces);
+
+            return (xamlContent, csContent);
+        }
+
+        private string BuildXamlDialog(CodeClass codeClass, Dictionary<string, string> namespaces, bool isUseDataBinding)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var controls = new StringBuilder();
+            foreach (CodeElement member in codeClass.Members)
+            {
+                if (member is CodeProperty property)
+                {
+                    var dataProperty = isUseDataBinding
+                        ? _propertyControlBuilder.BuildDataBoundPropertyControl(property) : _propertyControlBuilder.BuildPropertyControl(property);
+                    controls.AppendLine(dataProperty);
+                }
+            }
+
+            return $@"<Window x:Class=""{namespaces["project"]}.Views.Dialog{codeClass.Name}View""
+                    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+                    xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+                    xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+                    mc:Ignorable=""d""
+                    Width=""400"">
+    <StackPanel Margin=""10"">
+        {controls}
+        <StackPanel Orientation=""Horizontal"" HorizontalAlignment=""Right"" Margin=""0,10,0,0"">
+            <Button Content=""Save"" Command=""{{Binding SaveCommand}}"" Width=""80"" Margin=""5""/>
+            <Button Content=""Cancel"" Command=""{{Binding CancelCommand}}"" Width=""80"" Margin=""5""/>
+        </StackPanel>
+    </StackPanel>
+</Window>";
+        }
+
+        private string BuildCsDialog(CodeClass codeClass, Dictionary<string, string> namespaces)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            return $@"using System.Windows;
+
+namespace {namespaces["project"]}.Views
+{{
+    public partial class Dialog{codeClass.Name}View : Window
+    {{
+        public Dialog{codeClass.Name}View()
+        {{
+            InitializeComponent();
+        }}
+    }}
+}}";
+        }
+    }
+}
---
+++ Services/Builders/DialogViewModelBuilder.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiplomProject.Services.Builder;
+using DiplomProject.Services.Resolver;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+
+namespace DiplomProject.Services.Builders
+{
+    public class DialogViewModelBuilder
+    {
+        private readonly NamespaceResolver _namespaceResolver = new NamespaceResolver();
+        private readonly PropertyCodeBuilder _propertyCodeBuilder = new PropertyCodeBuilder();
+
+        public string BuildDialogViewModelContent(CodeClass codeClass)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var namespaces = _namespaceResolver.GetProjectNamespace(codeClass);
+
+            var propertyCode = new StringBuilder();
+
+            BuildProperty(codeClass, propertyCode);
+
+            return $@"using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using {namespaces["model"]};
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+namespace {namespaces["project"]}.ViewModels
+{{
+    public class Dialog{codeClass.Name}ViewModel : INotifyPropertyChanged
+    {{
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private {codeClass.Name} _item;
+        private string _windowTitle;
+
+        public string WindowTitle
+        {{
+            get => _windowTitle;
+            set
+            {{
+                _windowTitle = value;
+                OnPropertyChanged();
+            }}
+        }}
+
+        {propertyCode}
+
+        public ICommand SaveCommand {{ get; }}
+        public ICommand CancelCommand {{ get; }}
+        public bool IsSaved {{ get; private set; }}
+
+        public Dialog{codeClass.Name}ViewModel({codeClass.Name} item = null, string title = ""Add Product"")
+        {{
+            _item = item ?? new {codeClass.Name}();
+            WindowTitle = title;
+            
+            SaveCommand = new RelayCommand(Save);
+            CancelCommand = new RelayCommand(Cancel);
+        }}
+
+        public {codeClass.Name} GetItem() => _item;
+
+        private void Save()
+        {{
+            IsSaved = true;
+            CloseWindow();
+        }}
+        
+        private void Cancel()
+        {{
+            IsSaved = false;
+            CloseWindow();
+        }}
+
+        private void CloseWindow()
+        {{
+            foreach (Window window in Application.Current.Windows)
+            {{
+                if (window.DataContext == this)
+                {{
+                    window.Close();
+                    break;
+                }}
+            }}
+        }}
+
+        protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {{
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }}
+    }}
+}}";
+        }
+
+        private void BuildProperty(CodeClass codeClass, StringBuilder propertyCode)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            foreach (CodeElement member in codeClass.Members)
+            {
+                if (member is CodeProperty property)
+                {
+                    propertyCode.AppendLine(_propertyCodeBuilder.BuildPropertyDialogCode(property, codeClass));
+                }
+            }
+        }
+    }
+}
